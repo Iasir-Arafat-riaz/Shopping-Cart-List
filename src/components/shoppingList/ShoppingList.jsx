29e2ff02057565d7e2617d1ecee9c45bac87878f
Iasir-Arafat-riaz <iasir.arafat.riaz@gmail.com>
2023-05-31 19:23:10 +0600

    final commit

--- src/components/shoppingList/ShoppingList.jsx
+++ src/components/shoppingList/ShoppingList.jsx
@@ -7,28 +7,32 @@ const ShoppingList = () => {
   const [totalCartCount, setTotalCartCount] = useState(0);
   const [itemAdd, setItemAdd] = useState("");
 
-  const itemCartAdd = () => {
+  const itemCartAdd = (e) => {
+    e.preventDefault()
     if (itemAdd.length === 0 || listItems.includes(itemAdd)) {
       return alert("Your Order duplicate or empty!");
-      }
-      
+    }
     setListItems([...listItems, itemAdd]);
+    setItemAdd("")
   };
   console.log("total count =", totalCartCount, "item add =", itemAdd);
-  
+
   return (
     <div className="list-item">
       {/* <button className='item-add-button' onClick={itemCartAdd}><div >Add an item...</div> <div>+</div></button> */}
-      <div className="add-item-field">
+
+      <form onSubmit={itemCartAdd} action="" className="add-item-field">
         <input
           type="text"
           name=""
           id=""
           placeholder="Add an item..."
           onChange={(e) => setItemAdd(e.target.value)}
+          value={itemAdd}
         />
-        <FaPlus className="item-add-button" onClick={itemCartAdd} />
-      </div>
+        <button type="submit" className="item-add-button"><FaPlus/></button>
+      </form>
+
       {listItems.map((item, index) => (
         <Cart key={index} item={item} setTotalCartCount={setTotalCartCount} />
       ))}

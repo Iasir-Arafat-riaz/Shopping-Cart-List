85aa56a40d23cddf2ee4943e3eed679980a18916
Iasir-Arafat-riaz <riaz@faqi.com.au>
2023-08-03 10:31:48 +0600

    adding delete functionality

--- src/components/shoppingList/ShoppingList.jsx
+++ src/components/shoppingList/ShoppingList.jsx
@@ -9,18 +9,23 @@ const ShoppingList = () => {
 
   const itemCartAdd = (e) => {
     e.preventDefault();
-    if (itemAdd.length === 0 || listItems.includes(itemAdd)) {
+    if (itemAdd.length === 0 || listItems.find(i => i.itemName === itemAdd)) {
       return alert("Your Order duplicate or empty!");
     }
-    setListItems([...listItems, itemAdd]);
+    setListItems([...listItems, { itemName: itemAdd, count: 0, isChecked: false, uniqId: Math.floor(Math.random() * 1000000) }]);
     setItemAdd("");
   };
 
   console.log("total count =", totalCartCount, "item add =", itemAdd);
+  console.log(listItems);
 
   return (
     <div className="list-item">
-      <form onSubmit={itemCartAdd} action="" className="add-item-field">
+      <form
+        onSubmit={itemCartAdd}
+        action=""
+        className="add-item-field"
+      >
         <input
           type="text"
           name=""
@@ -29,14 +34,18 @@ const ShoppingList = () => {
           onChange={(e) => setItemAdd(e.target.value)}
           value={itemAdd}
         />
-        <button type="submit" className="item-add-button">
+        <button
+          type="submit"
+          className="item-add-button"
+        >
           <FaPlus />
         </button>
       </form>
 
       {listItems.map((item, index) => (
-        <Cart key={index} item={item} setTotalCartCount={setTotalCartCount} />
+        <Cart key={index} item={item} setListItems={setListItems} setTotalCartCount={setTotalCartCount} listItems={listItems} />
       ))}
+
       <div className="total-cart">
         Total : {totalCartCount}
       </div>
--- src/components/shoppingList/parts/Cart.jsx
+++ src/components/shoppingList/parts/Cart.jsx
@@ -2,67 +2,70 @@ import React, { useState } from "react";
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
 
+const Cart = ({ item, listItems, setListItems, setTotalCartCount }) => {
 
-
-const Cart = ({ item, setTotalCartCount }) => {
-  const [cartCount, setCartCount] = useState(0);
-  const [change, setChange] = useState(false);
-
-  console.log(change);
+  console.log(item.isChecked);
 
   const handleChange = () => {
-    setChange(!change)
+    item.isChecked = !item.isChecked;
 
-    console.log(change);
+    console.log(item.isChecked);
 
-    if (change) {
-      setTotalCartCount(pre => pre + cartCount)
+    if (item.isChecked) {
+      setTotalCartCount(pre => pre - item.count)
     }
-    else if (!change) {
-      setTotalCartCount(pre => pre - cartCount)
+    else if (!item.isChecked) {
+      setTotalCartCount(pre => pre + item.count)
     }
   }
-
-  console.log(cartCount);
-
-  if (cartCount < 0) {
-    setCartCount(0);
+  const removeItem = (ui) => {
+    console.log("remove item");
+    const filteredItems = listItems.filter(item => item.uniqId !== ui)
+    setListItems(filteredItems)
+    const findingItem = listItems.find(i => i.uniqId == ui);
+    const removedCount = findingItem.count;
+    console.log(removedCount);
+    setTotalCartCount(pre => pre - removedCount)
   }
 
   return (
     <div className="item-cart">
       <div className="item-number">
-        <input type="checkbox" id="checkbox" onChange={handleChange} />
+        <input
+          type="checkbox"
+          id="checkbox"
+          onChange={handleChange}
+        />
         <label htmlFor="checkbox">
-          {item}
+          {item.itemName}
         </label>
       </div>
+      <button onClick={() => removeItem(item.uniqId)}>x</button>
       <div className="cart-count">
-        <button disabled={change}>
+        <button disabled={item.isChecked}>
           <FaChevronLeft
             className="decrease"
             onClick={() => {
-              if (change) {
+              if (item.count === 0) {
                 return;
               }
-              setCartCount((pre) => pre - 1);
-              if (cartCount === 0) {
+              if (item.isChecked) {
                 return;
               }
+              item.count--;
               setTotalCartCount((pre) => pre - 1);
             }}
           />
           <span className="cart-count-value">
-            {cartCount}
+            {item.count}
           </span>
-
           <FaChevronRight
             className="increase"
             onClick={() => {
-              if (change) {
+              if (item.isChecked) {
                 return;
               }
-              setCartCount((pre) => pre + 1);
+              item.count++
               setTotalCartCount((pre) => pre + 1);
             }}
           />
